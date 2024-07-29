4106ca57af4617f7a796354fc8be322c486bec55
envin <envin@pm.me>
2024-07-30 00:38:42 +0200

    feat(ptokens-constants)!: drop NetworkId

--- packages/ptokens-constants/src/index.ts
+++ packages/ptokens-constants/src/index.ts
@@ -1,4 +1,4 @@
-export enum NetworkId {
+export enum ChainId {
   SepoliaTestnet = '0xaa36a7',
 }
 
@@ -9,34 +9,8 @@ export enum BlockchainType {
   ALGORAND,
 }
 
-export const networkIdToTypeMap = new Map<string, BlockchainType>([[NetworkId.SepoliaTestnet, BlockchainType.EVM]])
+export const networkIdToTypeMap = new Map<string, BlockchainType>([[ChainId.SepoliaTestnet, BlockchainType.EVM]])
 
-export enum Blockchain {
-  Ethereum,
-  Sepolia,
-  Goerli,
-  Bitcoin,
-  Eos,
-  Telos,
-  Bsc,
-  Xdai,
-  Polygon,
-  Ultra,
-  Fio,
-  Arbitrum,
-  Luxochain,
-  Fantom,
-  Algorand,
-  Libre,
-  Litecoin,
-  Gnosis,
-}
-
-export enum Network {
-  Mainnet,
-  Testnet,
-}
-
-export const AdapterAddress = new Map<NetworkId, string>([
-  [NetworkId.SepoliaTestnet, '0x87415715056DA7A5EB1a30E53C4F4d20B44DB71D'],
+export const AdapterAddress = new Map<ChainId, string>([
+  [ChainId.SepoliaTestnet, '0x87415715056DA7A5EB1a30E53C4F4d20B44DB71D'],
 ])
