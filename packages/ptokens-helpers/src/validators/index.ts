b99121be064f9319916f4ccc6b2d43fd51449ebd
envin <envin@pm.me>
2024-08-09 02:47:29 +0200

    refactor(ptokens-helpers): use BlockchainType instead of ChainId

--- packages/ptokens-helpers/src/validators/index.ts
+++ packages/ptokens-helpers/src/validators/index.ts
@@ -1,17 +1,17 @@
 import { validate } from 'multicoin-address-validator'
-import { ChainId } from 'ptokens-constants'
+import { BlockchainType } from 'ptokens-constants'
 
 const validatorFunction =
   (_blockchain: string, _network = 'prod') =>
   (_address: string) =>
     validate(_address, _blockchain, _network)
 
-export const chainIdToAddressValidatorMap: Map<ChainId, (_address: string) => boolean> = new Map([
-  [ChainId.SepoliaTestnet, validatorFunction('eth')],
+export const chainTypeToAddressValidatorMap: Map<BlockchainType, (_address: string) => boolean> = new Map([
+  [BlockchainType.EVM, validatorFunction('eth')],
 ])
 
-export function isValidAddressByChainId(_address: string, _networkId: ChainId) {
-  const validator = chainIdToAddressValidatorMap.get(_networkId)
+export function isValidAddressByChainId(_address: string, _chaintype: BlockchainType) {
+  const validator = chainTypeToAddressValidatorMap.get(_chaintype)
   if (validator) return validator(_address)
   throw new Error('Missing address validator')
 }
