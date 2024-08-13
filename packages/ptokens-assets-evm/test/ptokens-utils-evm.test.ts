280c79f2e95bbef9a9ec6f1164b5bd69e6fd5ca6
envin <envin@pm.me>
2024-08-13 12:04:26 +0200

    test(ptokens-asset-evm): add test utils

--- packages/ptokens-assets-evm/test/ptokens-utils-evm.test.ts
+++ packages/ptokens-assets-evm/test/ptokens-utils-evm.test.ts
@@ -1,87 +1,110 @@
-import BigNumber from 'bignumber.js'
-import { NetworkId } from 'ptokens-constants'
-import { stringUtils } from 'ptokens-helpers'
-import { Log } from 'viem'
-import { polygon } from 'viem/chains'
+import { concat, Log, pad, sha256 } from 'viem'
+import { mainnet } from 'viem/chains'
 
-import PNetworkHubAbi from '../src/abi/PNetworkHubAbi'
 import * as utils from '../src/lib'
 
+import { eap } from './utils/eventAttestatorProof'
 import logs from './utils/logs.json'
 
 describe('ethereum utilities', () => {
-  test('Should return a Viem chain', () => {
-    require('ptokens-constants').INTERIM_NETWORK_ID = NetworkId.PolygonMainnet
-    const chain = utils.getViemChain(NetworkId.PolygonMainnet)
-    expect(chain).toBe(polygon)
-  })
+  describe('getEventPayload', () => {
+    it('should return the correct payload when the input log has a valid address and topics', () => {
+      const log: Log = logs[0] as unknown as Log
 
-  test('Should throw if NetworkId is not supported', () => {
-    try {
-      void utils.getViemChain('mockedNetworkId' as NetworkId)
-      fail()
-    } catch (_err) {
-      if (!(_err instanceof Error)) throw new Error('Invalid Error type')
-      expect(_err.message).toEqual('mockedNetworkId is not supported by ptokens.js')
-    }
-  })
-
-  test('Should return the correct Ethereum off-chain format', () => {
-    const onChainAmount = 10000
-    const decimals = 4
-    const expectedOffChainAmount = BigNumber(1)
-    const offChainAmount = utils.offChainFormat(onChainAmount, decimals)
-    expect(offChainAmount).toStrictEqual(expectedOffChainAmount)
-  })
-
-  test('Should return the correct Ethereum on-chain format', () => {
-    const offChainAmount = BigNumber(1)
-    const decimals = 4
-    const expectedOnChainAmount = BigNumber(10000)
-    const onChainAmount = utils.onChainFormat(offChainAmount, decimals)
-    expect(onChainAmount).toStrictEqual(expectedOnChainAmount)
-  })
-
-  test('Should return true since 0xhello is 0x prefixed', () => {
-    const string0xPrefixed = '0xhello'
-    const result = stringUtils.isHexPrefixed(string0xPrefixed)
-    expect(result).toBe(true)
-  })
-
-  test('Should return false since hello is not 0x prefixed', () => {
-    const string0xNotPrefixed = 'hello0x'
-    const result = stringUtils.isHexPrefixed(string0xNotPrefixed)
-    expect(result).toBe(false)
-  })
-
-  test('Should get operation ID from log', async () => {
-    const res = await Promise.allSettled(
-      logs
-        .slice(0, 3)
-        .concat(logs.slice(-3))
-        .map(
-          (_log) =>
-            new Promise((_resolve) =>
-              _resolve(utils.getOperationIdFromLog(_log as unknown as Log<bigint>, '0xf9b459a1' as NetworkId)),
-            ),
+      const expectedPayload = concat([
+        pad('0x45009dd3abbe29db54fc5d893ceaa98a624882df'),
+        sha256(
+          concat([
+            '0x9b706941b48091a1c675b439064f40b9d43c577d9c7134cce93179b9b0bf2a52',
+            '0x0000000000000000000000000000000000000000000000000000000000000000',
+          ]),
         ),
-    )
-    const expectedRes = Array(3)
-      .fill('0xb68e25afc0680bd3930459e5cfd3bc5b4cc0c07a67cfab9433a3d9337b2996ca')
-      .concat(Array(3).fill('0xc3e33a15fb36d4c813c32d85e8005baf94b37d032c9830f00009aa536966e5b3'))
-    expect(
-      res.map((_obj) => ('value' in _obj ? _obj.value : 'reason' in _obj ? (_obj.reason.message as string) : null)),
-    ).toStrictEqual(expectedRes)
+        '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000006379ebd504941f50d5bfde9348b37593bd29c835000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000051b716b3f6748000000000000000000000000000a0ee7a142d267c1f36714e4a8f75612f20a79720000000000000000000000000000000000000000000000000000000000000002a307866333946643665353161616438384636463463653661423838323732373963666646623932323636',
+      ])
+
+      expect(utils.getEventPayload(log)).toEqual(expectedPayload)
+    })
   })
 
-  test('Abi operation struct is consistent', () => {
-    const operationQueuedStruct = PNetworkHubAbi.find((_) => _.type === 'event' && _.name === 'OperationQueued')?.inputs
-    const operationExecutedStruct = PNetworkHubAbi.find((_) => _.type === 'event' && _.name === 'OperationExecuted')
-      ?.inputs
-    const operationCanceledFinalizedStruct = PNetworkHubAbi.find(
-      (_) => _.type === 'event' && _.name === 'OperationCancelFinalized',
-    )?.inputs
-    expect(operationQueuedStruct).toStrictEqual(operationExecutedStruct)
-    expect(operationExecutedStruct).toStrictEqual(operationCanceledFinalizedStruct)
+  describe('getEventPreImage', () => {
+    it('should throw if blockHash is not in log', () => {
+      try {
+        const log: Log = logs[0] as unknown as Log
+        utils.getEventPreImage({ ...log, blockHash: null }, eap.context as `0x${string}`)
+        fail()
+      } catch (_err) {
+        if (!(_err instanceof Error)) throw new Error('Invalid Error type')
+        expect(_err.message).toStrictEqual('Missing block hash')
+      }
+    })
+
+    it('should throw if transactionHash is not in log', () => {
+      try {
+        const log: Log = logs[0] as unknown as Log
+        utils.getEventPreImage({ ...log, transactionHash: null }, eap.context as `0x${string}`)
+        fail()
+      } catch (_err) {
+        if (!(_err instanceof Error)) throw new Error('Invalid Error type')
+        expect(_err.message).toStrictEqual('Missing transaction hash')
+      }
+    })
+
+    it('should return the preimage given log and context', () => {
+      const log: Log = logs[0] as unknown as Log
+      const preimage = utils.getEventPreImage(log, eap.context as `0x${string}`)
+      expect(preimage).toEqual(eap.preimage)
+    })
+  })
+
+  describe('getSwapEventId', () => {
+    it('should return the correct eventId when log and context are provided', () => {
+      const log: Log = logs[0] as unknown as Log
+      const eventId = utils.getSwapEventId(log, eap.context as `0x${string}`)
+      expect(eventId).toEqual(eap.eventid)
+    })
+  })
+
+  describe('getOperationFromLog', () => {
+    it('should return the correct eventId when log and context are provided', () => {
+      const log: Log = logs[0] as unknown as Log
+
+      const expectedOperation = {
+        blockId: '0x4c0a6d1927db6747ec3960cffe4dc9de0c4b1e2961cdc17b7f79d6a450306b7f',
+        txId: '0xf353c5ad5c0fa3d69a340dcc1c09d77d205e0ef2d6d20318b795390cea947a01',
+        nonce: 0n,
+        erc20: '0x0000000000000000000000006379ebd504941f50d5bfde9348b37593bd29c835',
+        originChainId: '0x0000000000000000000000000000000000000000000000000000000000000001',
+        destinationChainId: '0x0000000000000000000000000000000000000000000000000000000000000001',
+        amount: 5888200000000000000n,
+        sender: '0x000000000000000000000000a0ee7a142d267c1f36714e4a8f75612f20a79720',
+        recipient: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+        data: '0x',
+      }
+
+      const operation = utils.getOperationFromLog(log, mainnet.id)
+      expect(operation).toEqual(expectedOperation)
+    })
+
+    it('should throw if blockHash is not in log', () => {
+      try {
+        const log: Log = logs[0] as unknown as Log
+        utils.getOperationFromLog({ ...log, blockHash: null }, mainnet.id)
+        fail()
+      } catch (_err) {
+        if (!(_err instanceof Error)) throw new Error('Invalid Error type')
+        expect(_err.message).toStrictEqual('blockHash not retreived correctly')
+      }
+    })
+
+    it('should throw if transactionHash is not in log', () => {
+      try {
+        const log: Log = logs[0] as unknown as Log
+        utils.getOperationFromLog({ ...log, transactionHash: null }, mainnet.id)
+        fail()
+      } catch (_err) {
+        if (!(_err instanceof Error)) throw new Error('Invalid Error type')
+        expect(_err.message).toStrictEqual('transactionHash not retreived correctly')
+      }
+    })
   })
 })
