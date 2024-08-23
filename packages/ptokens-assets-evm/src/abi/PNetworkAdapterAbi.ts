2f685678ce7a02ff4cd4f7e436d87b83f5d2a4cb
envin <envin@pm.me>
2024-08-23 13:06:39 +0200

    test(ptokens-asset-evm): update utils test

--- packages/ptokens-assets-evm/src/abi/PNetworkAdapterAbi.ts
+++ packages/ptokens-assets-evm/src/abi/PNetworkAdapterAbi.ts
@@ -1,572 +1,468 @@
 export default [
   {
+    type: 'constructor',
     inputs: [
       {
-        internalType: 'address',
         name: '_xerc20',
         type: 'address',
+        internalType: 'address',
       },
       {
-        internalType: 'address',
         name: '_erc20',
         type: 'address',
+        internalType: 'address',
       },
     ],
     stateMutability: 'nonpayable',
-    type: 'constructor',
   },
   {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'target',
-        type: 'address',
-      },
-    ],
-    name: 'AddressEmptyCode',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
-    name: 'AddressInsufficientBalance',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'bytes32',
-        name: 'operationId',
-        type: 'bytes32',
-      },
-    ],
-    name: 'AlreadyProcessed',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'FailedInnerCall',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'fees',
-        type: 'uint256',
-      },
-    ],
-    name: 'InsufficientAmount',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'InvalidAmount',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'destinationChainId',
-        type: 'uint256',
-      },
-    ],
-    name: 'InvalidDestinationChainId',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'length',
-        type: 'uint256',
-      },
-    ],
-    name: 'InvalidEventContentLength',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'InvalidFeesManager',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'actual',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'expected',
-        type: 'uint256',
-      },
-    ],
-    name: 'InvalidMessageId',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'InvalidOperation',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'InvalidSender',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'InvalidSwap',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'token',
-        type: 'address',
-      },
-    ],
-    name: 'InvalidTokenAddress',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'NotAllowed',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'owner',
-        type: 'address',
-      },
-    ],
-    name: 'OwnableInvalidOwner',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
-    name: 'OwnableUnauthorizedAccount',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'RLPInputTooLong',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'token',
-        type: 'address',
-      },
-    ],
-    name: 'SafeERC20FailedOperation',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'bytes32',
-        name: 'eventId',
-        type: 'bytes32',
-      },
-    ],
-    name: 'Unauthorized',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'bytes32',
-        name: 'topic',
-        type: 'bytes32',
-      },
-    ],
-    name: 'UnexpectedEventTopic',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'chainId',
-        type: 'uint256',
-      },
-    ],
-    name: 'UnsupportedChainId',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'bytes1',
-        name: 'protocolId',
-        type: 'bytes1',
-      },
-    ],
-    name: 'UnsupportedProtocolId',
-    type: 'error',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'previousOwner',
-        type: 'address',
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
-    ],
-    name: 'OwnershipTransferred',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [],
-    name: 'ReceiveUserDataFailed',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'bytes32',
-        name: 'eventId',
-        type: 'bytes32',
-      },
-    ],
-    name: 'Settled',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'uint256',
-        name: 'nonce',
-        type: 'uint256',
-      },
-      {
-        components: [
-          {
-            internalType: 'bytes',
-            name: 'content',
-            type: 'bytes',
-          },
-        ],
-        indexed: false,
-        internalType: 'struct IAdapter.EventBytes',
-        name: 'eventBytes',
-        type: 'tuple',
-      },
-    ],
-    name: 'Swap',
-    type: 'event',
-  },
-  {
-    inputs: [],
-    name: 'SWAP_EVENT_TOPIC',
-    outputs: [
-      {
-        internalType: 'bytes32',
-        name: '',
-        type: 'bytes32',
-      },
-    ],
-    stateMutability: 'view',
     type: 'function',
-  },
-  {
-    inputs: [],
     name: 'erc20',
-    outputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
     inputs: [],
-    name: 'owner',
     outputs: [
       {
-        internalType: 'address',
         name: '',
         type: 'address',
+        internalType: 'address',
       },
     ],
     stateMutability: 'view',
-    type: 'function',
   },
   {
+    type: 'function',
+    name: 'owner',
+    inputs: [],
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    name: 'pastEvents',
     inputs: [
       {
-        internalType: 'bytes32',
         name: '',
         type: 'bytes32',
+        internalType: 'bytes32',
       },
     ],
-    name: 'pastEvents',
     outputs: [
       {
-        internalType: 'bool',
         name: '',
         type: 'bool',
+        internalType: 'bool',
       },
     ],
     stateMutability: 'view',
-    type: 'function',
   },
   {
-    inputs: [],
+    type: 'function',
     name: 'renounceOwnership',
+    inputs: [],
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
   },
   {
+    type: 'function',
+    name: 'settle',
     inputs: [
       {
-        components: [
-          {
-            internalType: 'bytes32',
-            name: 'blockId',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'txId',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'uint256',
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'erc20',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'originChainId',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'destinationChainId',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'uint256',
-            name: 'amount',
-            type: 'uint256',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'sender',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'address',
-            name: 'recipient',
-            type: 'address',
-          },
-          {
-            internalType: 'bytes',
-            name: 'data',
-            type: 'bytes',
-          },
-        ],
-        internalType: 'struct IAdapter.Operation',
         name: 'operation',
         type: 'tuple',
-      },
-      {
+        internalType: 'struct IAdapter.Operation',
         components: [
           {
-            internalType: 'bytes',
-            name: 'preimage',
-            type: 'bytes',
+            name: 'blockId',
+            type: 'bytes32',
+            internalType: 'bytes32',
           },
           {
-            internalType: 'bytes',
-            name: 'signature',
+            name: 'txId',
+            type: 'bytes32',
+            internalType: 'bytes32',
+          },
+          {
+            name: 'nonce',
+            type: 'uint256',
+            internalType: 'uint256',
+          },
+          {
+            name: 'erc20',
+            type: 'bytes32',
+            internalType: 'bytes32',
+          },
+          {
+            name: 'originChainId',
+            type: 'bytes32',
+            internalType: 'bytes32',
+          },
+          {
+            name: 'destinationChainId',
+            type: 'bytes32',
+            internalType: 'bytes32',
+          },
+          {
+            name: 'amount',
+            type: 'uint256',
+            internalType: 'uint256',
+          },
+          {
+            name: 'sender',
+            type: 'bytes32',
+            internalType: 'bytes32',
+          },
+          {
+            name: 'recipient',
+            type: 'address',
+            internalType: 'address',
+          },
+          {
+            name: 'data',
             type: 'bytes',
+            internalType: 'bytes',
           },
         ],
-        internalType: 'struct IPAM.Metadata',
+      },
+      {
         name: 'metadata',
         type: 'tuple',
+        internalType: 'struct IPAM.Metadata',
+        components: [
+          {
+            name: 'preimage',
+            type: 'bytes',
+            internalType: 'bytes',
+          },
+          {
+            name: 'signature',
+            type: 'bytes',
+            internalType: 'bytes',
+          },
+        ],
       },
     ],
-    name: 'settle',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
   },
   {
+    type: 'function',
+    name: 'swap',
     inputs: [
       {
-        internalType: 'address',
         name: 'token',
         type: 'address',
+        internalType: 'address',
       },
       {
-        internalType: 'uint256',
         name: 'amount',
         type: 'uint256',
+        internalType: 'uint256',
       },
       {
-        internalType: 'uint256',
         name: 'destinationChainId',
         type: 'uint256',
+        internalType: 'uint256',
       },
       {
-        internalType: 'string',
         name: 'recipient',
         type: 'string',
-      },
-    ],
-    name: 'swap',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'token',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'destinationChainId',
-        type: 'uint256',
-      },
-      {
         internalType: 'string',
-        name: 'recipient',
-        type: 'string',
       },
       {
-        internalType: 'bytes',
         name: 'data',
         type: 'bytes',
-      },
-    ],
-    name: 'swap',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'destinationChainId',
-        type: 'uint256',
-      },
-      {
-        internalType: 'string',
-        name: 'recipient',
-        type: 'string',
-      },
-      {
         internalType: 'bytes',
-        name: 'data',
-        type: 'bytes',
       },
     ],
-    name: 'swapNative',
     outputs: [],
     stateMutability: 'payable',
-    type: 'function',
   },
   {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'destinationChainId',
-        type: 'uint256',
-      },
-      {
-        internalType: 'string',
-        name: 'recipient',
-        type: 'string',
-      },
-    ],
-    name: 'swapNative',
-    outputs: [],
-    stateMutability: 'payable',
     type: 'function',
-  },
-  {
+    name: 'transferOwnership',
     inputs: [
       {
-        internalType: 'address',
         name: 'newOwner',
         type: 'address',
+        internalType: 'address',
       },
     ],
-    name: 'transferOwnership',
     outputs: [],
     stateMutability: 'nonpayable',
-    type: 'function',
   },
   {
-    inputs: [],
+    type: 'function',
     name: 'xerc20',
+    inputs: [],
     outputs: [
       {
-        internalType: 'address',
         name: '',
         type: 'address',
+        internalType: 'address',
       },
     ],
     stateMutability: 'view',
-    type: 'function',
+  },
+  {
+    type: 'event',
+    name: 'OwnershipTransferred',
+    inputs: [
+      {
+        name: 'previousOwner',
+        type: 'address',
+        indexed: true,
+        internalType: 'address',
+      },
+      {
+        name: 'newOwner',
+        type: 'address',
+        indexed: true,
+        internalType: 'address',
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: 'event',
+    name: 'ReceiveUserDataFailed',
+    inputs: [],
+    anonymous: false,
+  },
+  {
+    type: 'event',
+    name: 'Settled',
+    inputs: [
+      {
+        name: 'eventId',
+        type: 'bytes32',
+        indexed: true,
+        internalType: 'bytes32',
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: 'event',
+    name: 'Swap',
+    inputs: [
+      {
+        name: 'nonce',
+        type: 'uint256',
+        indexed: true,
+        internalType: 'uint256',
+      },
+      {
+        name: 'eventBytes',
+        type: 'tuple',
+        indexed: false,
+        internalType: 'struct IAdapter.EventBytes',
+        components: [
+          {
+            name: 'content',
+            type: 'bytes',
+            internalType: 'bytes',
+          },
+        ],
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: 'error',
+    name: 'AlreadyProcessed',
+    inputs: [
+      {
+        name: 'operationId',
+        type: 'bytes32',
+        internalType: 'bytes32',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'InsufficientAmount',
+    inputs: [
+      {
+        name: 'amount',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: 'fees',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'InvalidAmount',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'InvalidDestinationChainId',
+    inputs: [
+      {
+        name: 'destinationChainId',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'InvalidEventContentLength',
+    inputs: [
+      {
+        name: 'length',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'InvalidFeesManager',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'InvalidMessageId',
+    inputs: [
+      {
+        name: 'actual',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: 'expected',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'InvalidOperation',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'InvalidSender',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'InvalidSwap',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'InvalidTokenAddress',
+    inputs: [
+      {
+        name: 'token',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'NotAllowed',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'OwnableInvalidOwner',
+    inputs: [
+      {
+        name: 'owner',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'OwnableUnauthorizedAccount',
+    inputs: [
+      {
+        name: 'account',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'RLPInputTooLong',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'ReentrancyGuardReentrantCall',
+    inputs: [],
+  },
+  {
+    type: 'error',
+    name: 'SafeERC20FailedOperation',
+    inputs: [
+      {
+        name: 'token',
+        type: 'address',
+        internalType: 'address',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'Unauthorized',
+    inputs: [
+      {
+        name: 'eventId',
+        type: 'bytes32',
+        internalType: 'bytes32',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'UnexpectedEventTopic',
+    inputs: [
+      {
+        name: 'topic',
+        type: 'bytes32',
+        internalType: 'bytes32',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'UnsupportedChainId',
+    inputs: [
+      {
+        name: 'chainId',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'error',
+    name: 'UnsupportedProtocolId',
+    inputs: [
+      {
+        name: 'protocolId',
+        type: 'bytes1',
+        internalType: 'bytes1',
+      },
+    ],
   },
 ] as const
