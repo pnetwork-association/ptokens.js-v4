b49b7227400812ce3a080e1e5a39b08ef5e7e8a9
envin <envin@pm.me>
2024-08-30 01:04:10 +0200

    feat(ptokens-constants): add bsc

--- packages/ptokens-constants/src/index.ts
+++ packages/ptokens-constants/src/index.ts
@@ -1,5 +1,6 @@
 export enum Chain {
   Mainnet = '0x1',
+  Bsc = '0x38',
   SepoliaTestnet = '0xaa36a7',
 }
 
@@ -20,6 +21,7 @@ export enum BlockchainType {
 }
 
 export const AdapterAddress = new Map<Chain, string>([
+  [Chain.Mainnet, '0x8ce361602B935680E8DeC218b820ff5056BeB7af'],
+  [Chain.Bsc, '0xb19b36b1456E65E3A6D514D3F715f204BD59f431'],
   [Chain.SepoliaTestnet, '0x87415715056DA7A5EB1a30E53C4F4d20B44DB71D'],
-  [Chain.Mainnet, '0x55555715056DA7A5EB1a30E53C4F4d20B4455555'], // FIXME: placeholder
 ])
