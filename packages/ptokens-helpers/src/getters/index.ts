35b9228ed5c1c6f14045634385a3e8ad93206712
envin <envin@pm.me>
2024-09-06 18:25:41 +0200

    feat(ptokens-helpers)!: update getters

--- packages/ptokens-helpers/src/getters/index.ts
+++ packages/ptokens-helpers/src/getters/index.ts
@@ -1,10 +1,9 @@
-import { AdapterAddress, Chain } from 'ptokens-constants'
+import { Chain } from 'ptokens-constants'
 
 import { addHexPrefix } from '../string'
 
-export function getAdapterAddress(chainId: string | number): string | undefined {
-  if (typeof chainId === 'number') {
-    chainId = addHexPrefix(chainId.toString(16))
-  }
-  return AdapterAddress.get(chainId as Chain)
+export function getChain(chainId: string | number): Chain {
+  const inputAsString = typeof chainId === 'number' ? addHexPrefix(chainId.toString(16)) : addHexPrefix(chainId)
+  if (Object.values(Chain).includes(inputAsString as Chain)) return inputAsString as Chain
+  throw new Error(`Invalid chain ID: ${inputAsString}`)
 }
